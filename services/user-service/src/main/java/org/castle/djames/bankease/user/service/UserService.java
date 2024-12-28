5e6d257b5d062126fce198594925a23a4c67bfe4
DJames <davejames1109@gmail.com>
2024-12-28 15:37:39 +0800

    WIP search

--- services/user-service/src/main/java/org/castle/djames/bankease/user/service/UserService.java
+++ services/user-service/src/main/java/org/castle/djames/bankease/user/service/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
---
+++ services/user-service/src/main/java/org/castle/djames/bankease/user/specification/UserSpecification.java
@@ -0,0 +1,26 @@
+package org.castle.djames.bankease.user.specification;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.castle.djames.bankease.user.entity.User;
+import org.springframework.data.jpa.domain.Specification;
+
+public record UserSpecification(SpecSearchCriteria criteria) implements Specification<User> {
+
+    @Override
+    public Predicate toPredicate(final Root<User> root, final CriteriaQuery<?> query, final CriteriaBuilder builder) {
+        return switch (criteria.getOperation()) {
+            case EQUALITY -> builder.equal(root.get(criteria.getKey()), criteria.getValue());
+            case NEGATION -> builder.notEqual(root.get(criteria.getKey()), criteria.getValue());
+            case GREATER_THAN -> builder.greaterThan(root.get(criteria.getKey()), criteria.getValue().toString());
+            case LESS_THAN -> builder.lessThan(root.get(criteria.getKey()), criteria.getValue().toString());
+            case LIKE -> builder.like(root.get(criteria.getKey()), criteria.getValue().toString());
+            case STARTS_WITH -> builder.like(root.get(criteria.getKey()), criteria.getValue() + "%");
+            case ENDS_WITH -> builder.like(root.get(criteria.getKey()), "%" + criteria.getValue());
+            case CONTAINS -> builder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%");
+        };
+    }
+
+}
