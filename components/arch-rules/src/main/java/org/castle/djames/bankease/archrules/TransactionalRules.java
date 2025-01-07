048ca616df81df50b9176e986d39804a1d7ba9cf
DJames <davejames1109@gmail.com>
2025-01-07 23:14:05 +0800

    Update transactional rules, add related to proxy

--- components/arch-rules/src/main/java/org/castle/djames/bankease/archrules/TransactionalRules.java
+++ components/arch-rules/src/main/java/org/castle/djames/bankease/archrules/TransactionalRules.java
@@ -2,6 +2,7 @@ package org.castle.djames.bankease.archrules;
 
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.ProxyRules;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
@@ -15,4 +16,15 @@ public class TransactionalRules {
     public ArchRule transactionalMethodsShouldBePublic = methods().that()
             .areAnnotatedWith(Transactional.class)
             .should().bePublic();
+
+    /**
+     * Enforces that methods annotated with @Transactional should not be directly
+     * called from other methods within the same class.
+     * <p>
+     * Direct calls within the same class bypass the proxy created by Spring,
+     * which is responsible for starting and managing transactions.
+     */
+    @ArchTest
+    public ArchRule transactionalMethodsShouldNotBeCalledFromTheSameClass =
+            ProxyRules.no_classes_should_directly_call_other_methods_declared_in_the_same_class_that_are_annotated_with(Transactional.class);
 }
